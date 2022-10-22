27eebe93e6c27b993bd469236c679f752147a8c5
CNOO <kcw9028@naver.com>
2022-10-22 10:57:42 +0900

    create project

---
+++ letusgo2022/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  let us: Go! 2022, fall
+//  ViewController.swift
+//
+//  Created by clyne.kim on 2022/10/22.
+//  Copyright 2022 clyne.kim All rights reserved
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
