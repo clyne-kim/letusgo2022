95a8ab04cf7d0576489e5245f6cbca01ad3ed646
clyne.kim <kcw9028@naver.com>
2022-10-22 19:35:55 +0900

    divide by service

---
+++ letusgo2022/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  let us: Go! 2022, fall
+//  MainViewModel.swift
+//
+//  Created by clyne.kim on 2022/10/22.
+//  Copyright 2022 clyne.kim All rights reserved
+//
+
+import Foundation
+
+struct MainViewModel {
+    
+}
---
+++ letusgo2022/Services/CosService.swift
@@ -0,0 +1,14 @@
+//
+//  let us: Go! 2022, fall
+//  CosService.swift
+//
+//  Created by clyne.kim on 2022/10/22.
+//  Copyright 2022 clyne.kim All rights reserved
+//
+    
+
+import Foundation
+
+struct CosService {
+    
+}
